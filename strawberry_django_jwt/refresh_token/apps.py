7274bdb804a375cec6d8bd605430b36901be8b12
KundaPanda <vojdoh@gmail.com>
2021-11-02 10:00:45 +0100

    Explicitly set app label

--- strawberry_django_jwt/refresh_token/apps.py
+++ strawberry_django_jwt/refresh_token/apps.py
@@ -4,4 +4,5 @@ from django.utils.translation import gettext_lazy as _
 
 class RefreshTokenConfig(AppConfig):
     name = "strawberry_django_jwt.refresh_token"
+    label = "refresh_token"
     verbose_name = _("Refresh token")
